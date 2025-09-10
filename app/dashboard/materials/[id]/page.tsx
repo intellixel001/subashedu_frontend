a8b3ae3b498735b9d81dda4ea997154e798213ea
intellixel001 <intellixel001@users.noreply.github.com>
2025-09-10 23:49:16 +0600

    aa

--- app/dashboard/materials/[id]/page.tsx
+++ app/dashboard/materials/[id]/page.tsx
@@ -6,33 +6,40 @@ interface PageParams {
   id: string;
 }
 
-interface PDFReaderProps {
-  params: PageParams;
-}
-
-export default async function PDFReader({ params }: PDFReaderProps) {
+// Next.js page function
+export default async function PDFReader({
+  params,
+}: {
+  params: PageParams; // conforming to PageProps
+}) {
   const { id } = params;
-  if (!id)
+
+  if (!id) {
     return (
       <div className="min-h-screen flex justify-center items-center">
         Material not found
       </div>
     );
+  }
 
   const studentObject = await getCurrentStudent();
-  if (!studentObject)
+
+  if (!studentObject) {
     return (
       <div className="min-h-screen flex justify-center items-center">
         Student not found
       </div>
     );
+  }
 
   const student = studentObject.data.student;
+
   const isPurchased =
     student.coursesEnrolled.some((course) => course.materials.includes(id)) ||
     student.materials.includes(id);
 
   if (!isPurchased) return <MaterialPurchaseForm materialId={id} />;
 
+  // Use the PDF Viewer component
   return <PDFViewerWrapper materialId={id} />;
 }
