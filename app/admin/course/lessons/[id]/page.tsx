7ca08ed24bb7ac2ee5ba2b95d190b92ac6b5c807
intellixel001 <intellixel001@users.noreply.github.com>
2025-09-15 00:13:24 +0600

    a

--- app/admin/course/lessons/[id]/page.tsx
+++ app/admin/course/lessons/[id]/page.tsx
@@ -1,6 +1,7 @@
+// Do NOT use "use client" here
 import LessonsClient from "./LessonsClient";
 
-// This receives `params` directly because it's a server component
+// This is a server component, so it can receive params
 export default function Page({ params }: { params: { id: string } }) {
   return <LessonsClient courseId={params.id} />;
 }
