2952b2f357efc0f14bfef269401aa809f1173e89
intellixel001 <intellixel001@users.noreply.github.com>
2025-09-19 00:22:12 +0600

    a

--- app/course/[id]/enrollment/page.tsx
+++ app/course/[id]/enrollment/page.tsx
@@ -6,13 +6,13 @@ import Purchase from "./Purchase";
 export default async function EnrollmentPage({
   params,
 }: {
-  params: { id: string };
+  params: { id: string }; // <- just object, no Promise
 }) {
-  const courseId = params.id;
+  const courseId = params.id; // this will correctly be '123' for /myroute/123/enrollment
   const studentObject = await getCurrentStudent();
 
   // Fetch course
-  const course = await getPublicSingleCourse(courseId); // <- fix typo
+  const course = await getPublicSingleCourse(courseId);
   if (!course)
     return <p className="text-gray-400 mt-20 text-center">Course not found</p>;
 
