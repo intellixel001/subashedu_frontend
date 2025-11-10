43348a69ca6f4b7634bc5ea2e01c1afc680b0376
intellixel001 <intellixel001@users.noreply.github.com>
2025-11-11 00:01:02 +0600

    a

--- app/components/Section12.tsx
+++ app/components/Section12.tsx
@@ -13,8 +13,8 @@ export default function Section12() {
               title="জানুন সুভাস এডু সম্পর্কে । আপনার মননের শিক্ষাকে জাগ্রত করাই আমাদের উদ্দেশ্য ।"
               frameBorder={0}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           </div>
 
